fix last tokens unmerged fan out gradients

src/transformers/models/llama/fan_out_restore_residuals_test.py
@@ -9,8 +9,9 @@ def test_fan_out_restore_residuals():
     hidden_states = torch.rand([3, 5, 960], device=device)
     hidden_states_clone = hidden_states.clone()
     residual_hidden_states_projection = torch.zeros_like(hidden_states)
+    attention_mask = torch.ones([ hidden_states.shape[0], hidden_states.shape[1] ], device=device, dtype=torch.long)
 
-    restored_hidden_states = fan_out_restore_residuals(merged_embeddings_counts, hidden_states, residual_hidden_states_projection)
+    restored_hidden_states = fan_out_restore_residuals(merged_embeddings_counts, hidden_states, residual_hidden_states_projection, attention_mask)
 
     assert (restored_hidden_states == hidden_states_clone).all()
 
@@ -33,7 +34,7 @@ def test_fan_out_restore_residuals_with_merging_map():
     hidden_states_clone = hidden_states.clone()
     residual_hidden_states_projection = torch.rand([1, merging_map.shape[1], 64], device=device, requires_grad=True)
 
-    restored_hidden_states = fan_out_restore_residuals(merged_embeddings_counts, hidden_states, residual_hidden_states_projection)
+    restored_hidden_states = fan_out_restore_residuals(merged_embeddings_counts, hidden_states, residual_hidden_states_projection, attention_mask)
 
     assert (restored_hidden_states[:, :2] == hidden_states_clone[:, :2]).all()
     assert (restored_hidden_states[:, 3:] == hidden_states_clone[:, 2:]).all()
@@ -47,5 +48,36 @@ def test_fan_out_restore_residuals_with_merging_map():
     assert (grad_init[:, 2, :] == residual_hidden_states_projection.grad[:, 2, :]).all()
     assert (grad_init[:, :2, :] == hidden_states.grad[:, :2, :]).all()
     assert (grad_init[:, 3, :] == hidden_states.grad[:, 2, :]).all()
+
+    breakpoint()
+
+
+def test_skip_last_tokens():
+    device = 'cuda'
+
+    merging_map = torch.tensor([ [ [ 0, 1 ], [ 0, 1 ], [ 1, 0 ], [ 1, 0 ] ] ], dtype=torch.bfloat16, device=device)
+    attention_mask = torch.ones([ 1, merging_map.shape[1] ], device=device, dtype=torch.bool)
+    special_embeddings_mask = torch.zeros([ 1, merging_map.shape[1] ], device=device, dtype=torch.bool)
+    special_embeddings_mask[:, 0] = 1
+
+    aggregated_embeddings_transform_output, merged_embeddings_counts, merged_attention_mask = generate_merges_transform(merging_map, attention_mask, special_embeddings_mask)
+
+    assert (merged_embeddings_counts == torch.tensor([ [ 1, 1 ] ], dtype=torch.long, device=device)).all()
+
+    hidden_states = torch.rand([1, merging_map[0, :, 1].sum().long().item(), 64], device=device, requires_grad=True)
+    hidden_states_clone = hidden_states.clone()
+    residual_hidden_states_projection = torch.rand([1, merging_map.shape[1], 64], device=device, requires_grad=True)
+
+    restored_hidden_states = fan_out_restore_residuals(merged_embeddings_counts, hidden_states, residual_hidden_states_projection, attention_mask)
+
+    assert (restored_hidden_states[:, :2] == hidden_states_clone[:, :2]).all()
+    assert (restored_hidden_states[:, 2:] == residual_hidden_states_projection[:, 2:]).all()
+
+    grad_init = torch.rand_like(restored_hidden_states)
+
+    restored_hidden_states.backward(grad_init)
+
+    assert (grad_init[:, :2, :] == hidden_states.grad[:, :2, :]).all()
+    assert (grad_init[:, 2:] == residual_hidden_states_projection.grad[:, 2:]).all()
 
     breakpoint()

src/transformers/models/llama/merges_transform/generate_merges/ops.py
@@ -130,6 +130,7 @@ def fan_out_restore_residuals(
         merged_embeddings_counts: Tensor, # [ bs, seq_len ]
         hidden_states: Tensor, # [ bs, new_seq_len, hidden_dim ]
         residual_hidden_states: Tensor, # [ bs, seq_len, hidden_dim ]
+        residual_hidden_states_attention_mask: Tensor, # [ bs, seq_len ]
         ) -> Tensor:
 
     torch._check(len(merged_embeddings_counts.shape) == 2)
@@ -148,8 +149,9 @@ def fan_out_restore_residuals(
         merged_embeddings_counts,
         hidden_states,
         residual_hidden_states,
+        residual_hidden_states_attention_mask,
     )
-    
+
     restored_hidden_states = restored_hidden_states.to(orig_dtype)
 
     return restored_hidden_states
@@ -158,14 +160,20 @@ def fan_out_restore_residuals(
 def _backward_fan_out_restore_residuals(ctx, restored_hidden_states_grad):
     # restored_hidden_states_grad ~ [ bs, seq_len, hidden_dim ]
     
-    (merged_embeddings_counts,) = ctx.saved_tensors
-    
+    (merged_embeddings_counts, residual_hidden_states_attention_mask) = ctx.saved_tensors
+        # [ bs ]
+    restored_hidden_states_seq_lengths = residual_hidden_states_attention_mask.sum(dim=-1)
+
+    print("restored_hidden_states_seq_lengths", restored_hidden_states_seq_lengths)
+    print("merged_embeddings_counts", merged_embeddings_counts)
+
     hidden_states_grad = None
     residual_hidden_states_grad = None
     if ctx.needs_input_grad[1] or ctx.needs_input_grad[2]:
         hidden_states_grad, residual_hidden_states_grad = torch.ops.generate_merges.backward_fan_out_restore_residuals.default(
             merged_embeddings_counts,
             restored_hidden_states_grad,
+            restored_hidden_states_seq_lengths,
         )
         
         # print("_backward_fan_out_restore_residuals hidden_states_grad", hidden_states_grad)
@@ -175,18 +183,18 @@ def _backward_fan_out_restore_residuals(ctx, restored_hidden_states_grad):
     assert residual_hidden_states_grad is not None
     assert residual_hidden_states_grad.shape == restored_hidden_states_grad.shape
     
-    return None, hidden_states_grad, residual_hidden_states_grad
+    return None, hidden_states_grad, residual_hidden_states_grad, None
 
 
 def _setup_context_fan_out_restore_residuals(ctx, inputs, output):
-    merged_embeddings_counts, hidden_states, residual_hidden_states = inputs
+    merged_embeddings_counts, hidden_states, residual_hidden_states, residual_hidden_states_attention_mask = inputs
 
     # if ctx.needs_input_grad[1]:
     #     saved_merging_map = merging_map
     # if ctx.needs_input_grad[2]:
     #     saved_merging_map = merging_map
 
-    ctx.save_for_backward(merged_embeddings_counts)
+    ctx.save_for_backward(merged_embeddings_counts, residual_hidden_states_attention_mask)
 
 
 torch.library.register_autograd(

src/transformers/models/llama/modeling_adaptive_llama.py
@@ -782,7 +782,7 @@ class AdaptiveFanOut(nn.Module):
         if self.fan_out_implementation in ('python',):
             restored_hidden_states = self._python_fan_out(batch_size, new_seq_len, hidden_states, merged_embeddings_counts, residual_hidden_states_projection)
         elif self.fan_out_implementation in ('cuda_kernel'):
-            restored_hidden_states = fan_out_restore_residuals(merged_embeddings_counts, hidden_states, residual_hidden_states_projection)
+            restored_hidden_states = fan_out_restore_residuals(merged_embeddings_counts, hidden_states, residual_hidden_states_projection, residual_attention_mask)
             if CHECK_WITH_PYTHON:
                 restored_hidden_states_py = self._python_fan_out(batch_size, new_seq_len, hidden_states, merged_embeddings_counts, residual_hidden_states_projection)
                 assert (restored_hidden_states_py == restored_hidden_states).all()
@@ -840,7 +840,7 @@ class AdaptiveFanOutHCG(nn.Module):
         residual_hidden_states_projection = self.fan_out_linear(residual_hidden_states)
 
         if not self.training:
-            hidden_states = fan_out_restore_residuals(merged_embeddings_counts, hidden_states, residual_hidden_states_projection)
+            hidden_states = fan_out_restore_residuals(merged_embeddings_counts, hidden_states, residual_hidden_states_projection, residual_attention_mask)
 
         hidden_states = hidden_states + residual_hidden_states_projection
 