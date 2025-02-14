fix fan out evaluation gradients

src/transformers/models/llama/benchmark_adaptive_llama.py
@@ -73,7 +73,7 @@ if __name__ == "__main__":
     llama_model.eval()
     print("llama model params:", count_params(llama_model))
 
-    tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+    tokenizer = AutoTokenizer.from_pretrained(checkpoint, padding_side='left')
 
     with torch.no_grad():
         if args.generate:
@@ -86,7 +86,7 @@ if __name__ == "__main__":
             #         current_model.train()
             for current_model in tqdm([model, llama_model]):
 
-                model_inputs = tokenizer([ '<|im_start|> Who are you?' ], return_tensors='pt')
+                model_inputs = tokenizer([ 'Question: Which US President was born Lesley Lynch King Jr?\nAnswer:', 'Question: How are you?\nAnswer:', ], return_tensors='pt', padding=True)
                 model_inputs = model_inputs.to(device)
 
                 print('model_inputs["input_ids"].shape', model_inputs['input_ids'].shape)

src/transformers/models/llama/fan_out_restore_residuals_test.py
@@ -36,49 +36,16 @@ def test_fan_out_restore_residuals_with_merging_map():
 
     restored_hidden_states = fan_out_restore_residuals(merged_embeddings_counts, hidden_states, residual_hidden_states_projection, attention_mask)
 
-    assert (restored_hidden_states[:, :2] == hidden_states_clone[:, :2]).all()
-    assert (restored_hidden_states[:, 3:] == hidden_states_clone[:, 2:]).all()
+    assert (restored_hidden_states[:, :3] == hidden_states_clone[:, :3]).all()
 
-    assert (restored_hidden_states[:, 2] == residual_hidden_states_projection[:, 2]).all()
-
-    grad_init = torch.rand_like(restored_hidden_states)
-
-    restored_hidden_states.backward(grad_init)
-
-    assert (grad_init[:, 2, :] == residual_hidden_states_projection.grad[:, 2, :]).all()
-    assert (grad_init[:, :2, :] == hidden_states.grad[:, :2, :]).all()
-    assert (grad_init[:, 3, :] == hidden_states.grad[:, 2, :]).all()
-
-    breakpoint()
-
-
-def test_skip_last_tokens():
-    device = 'cuda'
-
-    merging_map = torch.tensor([ [ [ 0, 1 ], [ 0, 1 ], [ 1, 0 ], [ 1, 0 ] ] ], dtype=torch.bfloat16, device=device)
-    attention_mask = torch.ones([ 1, merging_map.shape[1] ], device=device, dtype=torch.bool)
-    special_embeddings_mask = torch.zeros([ 1, merging_map.shape[1] ], device=device, dtype=torch.bool)
-    special_embeddings_mask[:, 0] = 1
-
-    aggregated_embeddings_transform_output, merged_embeddings_counts, merged_attention_mask = generate_merges_transform(merging_map, attention_mask, special_embeddings_mask)
-
-    assert (merged_embeddings_counts == torch.tensor([ [ 1, 1 ] ], dtype=torch.long, device=device)).all()
-
-    hidden_states = torch.rand([1, merging_map[0, :, 1].sum().long().item(), 64], device=device, requires_grad=True)
-    hidden_states_clone = hidden_states.clone()
-    residual_hidden_states_projection = torch.rand([1, merging_map.shape[1], 64], device=device, requires_grad=True)
-
-    restored_hidden_states = fan_out_restore_residuals(merged_embeddings_counts, hidden_states, residual_hidden_states_projection, attention_mask)
-
-    assert (restored_hidden_states[:, :2] == hidden_states_clone[:, :2]).all()
-    assert (restored_hidden_states[:, 2:] == residual_hidden_states_projection[:, 2:]).all()
+    assert (restored_hidden_states[:, -1] == residual_hidden_states_projection[:, -1]).all()
 
     grad_init = torch.rand_like(restored_hidden_states)
 
     restored_hidden_states.backward(grad_init)
 
-    assert (grad_init[:, :2, :] == hidden_states.grad[:, :2, :]).all()
-    assert (grad_init[:, 2:] == residual_hidden_states_projection.grad[:, 2:]).all()
+    assert (grad_init[:, -1, :] == residual_hidden_states_projection.grad[:, -1, :]).all()
+    assert (grad_init[:, :3, :] == hidden_states.grad[:, :3, :]).all()
 
     breakpoint()
 
@@ -115,7 +82,7 @@ def test_prune_tokens_concrete_middle():
 
     device = 'cuda'
 
-    hidden_state = torch.rand([ batch_size, seq_len, hidden_dim ], device=device)
+    hidden_state = torch.rand([ batch_size, seq_len, hidden_dim ], device=device, requires_grad=True)
     concrete_bool = torch.tensor([[ 1, 1, 1, 1, 0, 0, 1 ]], dtype=torch.bool, device=device)
     attention_mask = torch.ones_like(concrete_bool)
 
@@ -125,7 +92,7 @@ def test_prune_tokens_concrete_middle():
         attention_mask,
     )
 
-    expected_merged_emb_counts = torch.tensor([[ 1, 1, 1, 1, 3 ]], dtype=torch.long, device=device)
+    expected_merged_emb_counts = torch.tensor([[ 1, 1, 1, 3, 1 ]], dtype=torch.long, device=device)
     expected_attention_mask = torch.ones_like(expected_merged_emb_counts)
 
     assert merged_hidden_state.shape[1] == 5
@@ -156,7 +123,7 @@ def test_prune_tokens_concrete_end():
         attention_mask,
     )
 
-    expected_merged_emb_counts = torch.tensor([[ 1, 1, 1, 1 ]], dtype=torch.long, device=device)
+    expected_merged_emb_counts = torch.tensor([[ 1, 1, 1, 4 ]], dtype=torch.long, device=device)
     expected_attention_mask = torch.ones_like(expected_merged_emb_counts)
 
     assert merged_hidden_state.shape[1] == 4

src/transformers/models/llama/modeling_adaptive_llama.py
@@ -570,10 +570,10 @@ class HardConcreteGate(nn.Module):
         # print('attention_mask.numel()', attention_mask.numel())
         # print('attention_mask.sum / numel', attention_mask.sum() / attention_mask.numel())
 
-        # if concrete.isnan().any():
-        #     print("found nan after hcg")
-        #     print(f"concrete mean={concrete.mean().item():.2f} max={concrete.max().item():.2f} min={concrete.min().item():.2f}")
-        #     breakpoint()
+        if concrete.isnan().any():
+            print("found nan after hcg")
+            print(f"concrete mean={concrete.mean().item():.2f} max={concrete.max().item():.2f} min={concrete.min().item():.2f}")
+            breakpoint()
 
 
         return concrete
@@ -857,6 +857,8 @@ class AdaptiveFanOut(nn.Module):
         else:
             residual_hidden_states_projection = residual_hidden_states
 
+        print("residual_hidden_states_projection", residual_hidden_states_projection.sum(-1))
+
         if self.fan_out_implementation in ('python',):
             restored_hidden_states = self._python_fan_out(batch_size, new_seq_len, hidden_states, merged_embeddings_counts, residual_hidden_states_projection)
         elif self.fan_out_implementation in ('cuda_kernel'):
@@ -897,6 +899,8 @@ class AdaptiveFanOutHCG(nn.Module):
     def __init__(self, config: LlamaConfig):
         super().__init__()
         self.hidden_size = config.hidden_size
+        self.projection_enabled: bool = config.fan_out_projection
+
         self.fan_out_linear = nn.Sequential(
             nn.Linear(self.hidden_size, self.hidden_size),
         )
@@ -915,7 +919,10 @@ class AdaptiveFanOutHCG(nn.Module):
             AdaptiveFanOutOutput: input hidden states
         """
 
-        residual_hidden_states_projection = self.fan_out_linear(residual_hidden_states)
+        if self.projection_enabled:
+            residual_hidden_states_projection = self.fan_out_linear(residual_hidden_states)
+        else:
+            residual_hidden_states_projection = residual_hidden_states
 
         if not self.training:
             # print("merged_embeddings_counts", merged_embeddings_counts.shape, merged_embeddings_counts)
@@ -926,8 +933,8 @@ class AdaptiveFanOutHCG(nn.Module):
             # print("residual_hidden_states_projection", residual_hidden_states_projection.shape)
             # assert (merged_embeddings_counts.sum(dim=-1) == residual_attention_mask.sum(dim=-1)).all()
             hidden_states = fan_out_restore_residuals(merged_embeddings_counts, hidden_states, residual_hidden_states_projection, residual_attention_mask)
-
-        hidden_states = hidden_states + residual_hidden_states_projection
+        else:
+            hidden_states = hidden_states + residual_hidden_states_projection
 
         return AdaptiveFanOutOutput(hidden_state=hidden_states)
 
@@ -1664,12 +1671,13 @@ class AdaptiveLlamaForCausalLM(AdaptiveLlamaPreTrainedModel, GenerationMixin):
         )
         return_dict = return_dict if return_dict is not None else self.config.use_return_dict
 
-        if special_embeddings_mask is None:
-            special_embeddings_mask = torch.zeros([input_ids.shape[0], input_ids.shape[1]], device=input_ids.device)
-            special_embeddings_mask[:, 0] = 1
-
         if attention_mask is None:
             attention_mask = torch.ones_like(input_ids, dtype=torch.long)
+
+        if special_embeddings_mask is None:
+            special_embeddings_mask = attention_mask.cumsum(dim=-1)
+            special_embeddings_mask[special_embeddings_mask > 1] = 0
+
 
         assert special_embeddings_mask is not None
 

src/transformers/models/llama/train_adaptive_llama.py
@@ -993,8 +993,8 @@ if __name__ == "__main__":
         project_name=trackers_project_name,
     )
 
-    # with torch.autograd.set_detect_anomaly(True):
-    trainer.train(
-        # resume_from_checkpoint="adaptive_gumbel_2-2_1.7B_model_my_checkpoint-4995",
-        # resume_from_checkpoint='adaptive_13-13_hcg_temp_5.0/checkpoint-4995/',
-    )
+    with torch.autograd.set_detect_anomaly(True):
+        trainer.train(
+            # resume_from_checkpoint="adaptive_gumbel_2-2_1.7B_model_my_checkpoint-4995",
+            # resume_from_checkpoint='adaptive_13-13_hcg_temp_5.0/checkpoint-4995/',
+        )