5c2ccba180bceab956e861cffe0a8767bbad81e1
d.tarasov <d.tarasov@airi.net>
2025-05-19 23:31:59 +0300

    add build vocab scripts and determine layers hop count automatically

--- README.md
+++ README.md
@@ -22,19 +22,52 @@ python setup.py build_ext --inplace
 
 # Statistival Pruning Vocabulary
 
-Compute pairwise hidden states similarities
+1. Compute pairwise hidden states similarities
 ```
 python src/transformers/models/llama/paper/calculate_hopping_potential.py --llama_checkpoint Qwen/Qwen2.5-7B --batch_size 4 --num_samples 1024
 python src/transformers/models/llama/paper/calculate_hopping_potential.py --llama_checkpoint unsloth/Meta-Llama-3.1-8B --batch_size 4 --num_samples 1024
 ```
 
+2. Compute plots and mintoken_potential_min_df_csv_path
+```
+# Llama3.1 8B
+python src/transformers/models/llama/paper/analyze_hopping_potential.py --per_token_hopping_potentials_path results/token_embeddings_hopping_potential/per_token_hopping_potentials_Meta-Llama-3.1-8B.pkl --token_occurences_path results/token_embeddings_hopping_potential/token_occurencies_Meta-Llama-3.1-8B.pkl --output_dir results/token_embeddings_hopping_potential --llama_checkpoint unsloth/Meta-Llama-3.1-8B
 
+# Qwen2.5-7B
+python src/transformers/models/llama/paper/analyze_hopping_potential.py --per_token_hopping_potentials_path results/token_embeddings_hopping_potential/per_token_hopping_potentials_Qwen2.5-7B.pkl --token_occurences_path results/token_embeddings_hopping_potential/token_occurencies_Qwen2.5-7B.pkl --output_dir results/token_embeddings_hopping_potential --llama_checkpoint Qwen/Qwen2.5-7B
+```
 
+4. Build Vocab And Recommended first layer for hopping and number of hopping layers
+
+```
+# Llama3.1 8B
+python src/transformers/models/llama/paper/build_statistical_pruning_vocabulary.py --llama_checkpoint unsloth/Meta-Llama-3.1-8B --output_dir results/token_embeddings_hopping_potential --token_potential_min_df_csv_path results/token_embeddings_hopping_potential/token_potential_min_Meta-Llama-3.1-8B.csv
+
+# Qwen2.5-7B
+python src/transformers/models/llama/paper/build_statistical_pruning_vocabulary.py --llama_checkpoint Qwen/Qwen2.5-7B --output_dir results/token_embeddings_hopping_potential --token_potential_min_df_csv_path results/token_embeddings_hopping_potential/token_potential_min_Qwen2.5-7B.csv
+```
+
+5. Build Model Checkpoint From pretrained vocabulary
+
+Hyperparameters should be set based on 4th step script output.
+
+```
+# Llama3.1-8B
+LLAMA31_8B_FAN_IN_INDEX=22
+LLAMA31_8B_FAN_OUT_INDEX=26
+python src/transformers/models/llama/paper/adaptive_llama_from_vocab.py --checkpoint_base_path unsloth/Meta-Llama-3.1-8B --fan_in_idx $LLAMA31_8B_FAN_IN_INDEX --fan_out_idx $LLAMA31_8B_FAN_OUT_INDEX --vocab_csv_path results/token_embeddings_hopping_potential/pruning_vocab_q1_Meta-Llama-3.1-8B.csv --output_dir ./adaptive_hcg_llama31_8B_l${LLAMA31_8B_FAN_IN_INDEX}-${LLAMA31_8B_FAN_OUT_INDEX}_analytical_pruning
+
+# Qwen2.5-7B
+QWEN25_7B_FAN_IN_INDEX=12
+QWEN25_7B_FAN_OUT_INDEX=5
+python src/transformers/models/llama/paper/adaptive_llama_from_vocab.py --checkpoint_base_path Qwen/Qwen2.5-7B --fan_in_idx $QWEN25_7B_FAN_IN_INDEX --fan_out_idx $QWEN25_7B_FAN_OUT_INDEX --vocab_csv_path results/token_embeddings_hopping_potential/pruning_vocab_q25.csv --output_dir ./adaptive_hcg_qwen25_7B_l${QWEN25_7B_FAN_IN_INDEX}-${QWEN25_7B_FAN_OUT_INDEX}_analytical_pruning
+```
 
 # Training
 
+TODO
+
 ```
-python ./src/transformers/models/llama/train_adaptive_llama.py --save_strategy steps --save_steps 1000 --generate_merges_transform_impl cuda_kernel --per_device_train_batch_size 16 --learning_rate 5e-05 --num_train_epochs 1 --seed 1008 --training_dataset smollm-corpus --model_type pretrained_checkpoint --llama_checkpoint ./adaptive_hcg_slm2_1.7B_pretrain_8/checkpoint-4993/ --dummy_adaptive_fan_in_layers_str 1,1,1,1,1,1,1,0,1,1,1,1 --full_unmerge_str 0,0,0,0,0,0,0,0,0,0,0,0 --full_unmerge_loss_weight 0.0 --adam_beta1 0.9 --adam_beta2 0.95 --lr_scheduler_type cosine --merging_type hcg --temperature_schedule 0 --freeze_lm_backbone 0 --fan_out_projection 1 --logging_steps 50 --warmup_steps 2000 --output_dir ./adaptive_hcg_slm2_1.7B_layersi_max_loss_1.25_fix_fanoutproj_8  --learnt_temperature 0 --select_train_dataset_items 300000 --eval_steps 250 --gumbel_tau 1.0 --weight_decay 0.1 --scale_not_pruned_gradients 0.0 --hcg_loss_weight 10 --ce_merging_loss_weight 0.0 --gumbel_loss_weight_dynamic 0 --hcg_loss_weight_dynamic 1 --dataloader_num_workers 0 --bf16 1 --torch_compile 1 --sparsity_level 0.0 --concrete_random_mask_proba 0 --lm_loss_max_value 1.25 --hcg_loss_max_value 0.0
 ```
 
 
--- src/transformers/models/llama/paper/analyze_hopping_potential.py
+++ src/transformers/models/llama/paper/analyze_hopping_potential.py
@@ -33,19 +33,19 @@ def parse_args():
     parser.add_argument("--per_token_hopping_potentials_path", type=str, required=True)
     parser.add_argument("--token_occurences_path", type=str, required=True)
     parser.add_argument("--min_frequency", type=int, default=10)
-    parser.add_argument("--max_tokens", type=int, default=10)
+    parser.add_argument("--max_tokens", type=int, default=None)
     parser.add_argument("--output_dir", type=str, required=True)
     return parser.parse_args()
 
 def load_data(args):
     tokenizer = AutoTokenizer.from_pretrained(args.llama_checkpoint)
-    
+
     with open(args.per_token_hopping_potentials_path, "rb") as f:
         per_token_hopping_potentials = pickle.load(f)
-    
+
     with open(args.token_occurences_path, "rb") as f:
         token_occurences = pickle.load(f)
-    
+
     return tokenizer, per_token_hopping_potentials, token_occurences
 
 def analyze_token_potentials(tokenizer, per_token_hopping_potentials, token_occurences, max_tokens):
@@ -78,7 +78,7 @@ def analyze_token_potentials(tokenizer, per_token_hopping_potentials, token_occu
 
     return token_potential_min_df
 
-def plot_log_freq_vs_hopping_potential(token_potential_min_df, output_dir):
+def plot_log_freq_vs_hopping_potential(token_potential_min_df, output_dir, model_name=""):
     log_freq = np.log10(token_potential_min_df['token_freq'])
 
     bin_step = 1
@@ -94,13 +94,13 @@ def plot_log_freq_vs_hopping_potential(token_potential_min_df, output_dir):
     plt.ylim(0, 4)
     plt.title('Mean Hopping Potential vs Log(Freq) Bins')
     plt.tight_layout()
-    file_path = os.path.join(output_dir, "boxplot_log_freq_bins_vs_mean_hopping_potential.png")
+    file_path = os.path.join(output_dir, f"boxplot_log_freq_bins_vs_mean_hopping_potential_{model_name}.png")
     plt.savefig(file_path)
     print(f"Saved boxplot to {file_path}")
     plt.close()
     return file_path
 
-def plot_hopping_potential_distribution(token_potential_min_df, output_dir):
+def plot_hopping_potential_distribution(token_potential_min_df, output_dir, model_name=""):
     H_cols = [col for col in token_potential_min_df.columns if col.startswith("H")]
     token_potential_min_df[H_cols].plot(kind='box', figsize=(15, 6))
     plt.xticks(rotation=90)
@@ -109,13 +109,13 @@ def plot_hopping_potential_distribution(token_potential_min_df, output_dir):
     plt.xlabel('Hidden states to hop from')
     plt.grid(True)
     plt.tight_layout()
-    file_path = os.path.join(output_dir, "hopping_potential_min_boxplot.png")
+    file_path = os.path.join(output_dir, f"hopping_potential_min_boxplot_{model_name}.png")
     plt.savefig(file_path)
     print(f"Saved boxplot to {file_path}")
     plt.close()
     return file_path
 
-def plot_weighted_hopping_potential_distribution(token_potential_min_df, output_dir):
+def plot_weighted_hopping_potential_distribution(token_potential_min_df, output_dir, model_name=""):
     weighted_H_cols = [col for col in token_potential_min_df.columns if col.startswith("weighted_H")]
     token_potential_min_df[weighted_H_cols].plot(kind='box', figsize=(15, 6))
     plt.xticks(rotation=90)
@@ -123,13 +123,13 @@ def plot_weighted_hopping_potential_distribution(token_potential_min_df, output_
     plt.xlabel('Hidden states to hop from')
     plt.grid(True)
     plt.tight_layout()
-    file_path = os.path.join(output_dir, "weighted_hopping_potential_min_boxplot.png")
+    file_path = os.path.join(output_dir, f"weighted_hopping_potential_min_boxplot_{model_name}.png")
     plt.savefig(file_path)
     print(f"Saved boxplot to {file_path}")
     plt.close()
     return file_path
 
-def plot_token_occurrences_distribution(token_occurences, output_dir):
+def plot_token_occurrences_distribution(token_occurences, output_dir, model_name=""):
     token_occurences_values = np.array(list(token_occurences.values()))
     plt.hist(token_occurences_values, bins=100, edgecolor='black')
     plt.xlabel('Log token occurences')
@@ -137,7 +137,7 @@ def plot_token_occurrences_distribution(token_occurences, output_dir):
     plt.xscale('log')
     plt.yscale('log')
     plt.title('Token occurences distribution')
-    file_path = os.path.join(output_dir, "token_occurences_distribution.png")
+    file_path = os.path.join(output_dir, f"token_occurences_distribution_{model_name}.png")
     plt.savefig(file_path)
     print(f"Saved token occurences distribution to {file_path}")
     plt.close()
@@ -147,61 +147,38 @@ def analyze_hopping_potentials(token_potential_min_df):
     H_cols = [col for col in token_potential_min_df.columns if col.startswith("H")]
     max_h_per_token = token_potential_min_df[H_cols].idxmax(axis=1)
     ranked_h = max_h_per_token.value_counts().sort_values(ascending=False).head(10)
-    
+
     log_freq = np.log10(token_potential_min_df['token_freq'])
     mean_hopping_potential_corr = token_potential_min_df['mean_hopping_potential'].corr(log_freq)
-    
+
     return ranked_h, mean_hopping_potential_corr
 
 def main():
     args = parse_args()
     tokenizer, per_token_hopping_potentials, token_occurences = load_data(args)
-    
+
+    model_name = args.llama_checkpoint.split("/")[-1]
+
     print("total tokens", len(token_occurences))
     print("total token occurences", sum(token_occurences.values()))
-    
-    max_tokens = min(args.max_tokens, len(token_occurences))
+
+    max_tokens = len(token_occurences)
+    if args.max_tokens is not None:
+        max_tokens = min(args.max_tokens, len(token_occurences))
+
     token_potential_min_df = analyze_token_potentials(tokenizer, per_token_hopping_potentials, token_occurences, max_tokens)
-    token_potential_min_df.to_csv(os.path.join(args.output_dir, "token_potential_min.csv"), index=False)
-    
-    ranked_h, mean_hopping_potential_corr = analyze_hopping_potentials(token_potential_min_df)
-    print("Ранжирование H* по количеству максимумов:")
-    print(ranked_h)
-    print(f"mean_hopping_potential and log freq corr: {mean_hopping_potential_corr}")
 
-    plot_log_freq_vs_hopping_potential(token_potential_min_df, args.output_dir)
+    file_path = os.path.join(args.output_dir, f"token_potential_min_{model_name}.csv")
+    token_potential_min_df.to_csv(file_path, index=False)
+    print(f"Saved token potential min to {file_path}")
 
-    plot_hopping_potential_distribution(token_potential_min_df, args.output_dir)
-    plot_weighted_hopping_potential_distribution(token_potential_min_df, args.output_dir)
+    plot_log_freq_vs_hopping_potential(token_potential_min_df, args.output_dir, model_name=model_name)
 
-    plot_token_occurrences_distribution(token_occurences, args.output_dir)
+    plot_hopping_potential_distribution(token_potential_min_df, args.output_dir, model_name=model_name)
+    plot_weighted_hopping_potential_distribution(token_potential_min_df, args.output_dir, model_name=model_name)
 
-    num_hop_layers = token_potential_min_df['H19'].value_counts().reset_index().iloc[0]['index']
-    print(f"\n\nnum_hop_layers: {num_hop_layers}")
+    plot_token_occurrences_distribution(token_occurences, args.output_dir, model_name=model_name)
 
-    filtered_df = token_potential_min_df[ token_potential_min_df['H19'] == num_hop_layers ]
-
-    vocab_q25 = filtered_df.sort_values(by='weighted_H19', ascending=False).head(int(0.25 * len(filtered_df)))
-    file_path = os.path.join(args.output_dir, "pruning_vocab_q25.csv")
-    vocab_q25['token_id'].to_csv(file_path, index=False)
-    print(f"Saved pruning vocab q25 to {file_path}. Vocab size: {len(vocab_q25)}")
-
-    vocab_q50 = filtered_df.sort_values(by='weighted_H19', ascending=False).head(int(0.5 * len(filtered_df)))
-    file_path = os.path.join(args.output_dir, "pruning_vocab_q50.csv")
-    vocab_q50['token_id'].to_csv(file_path, index=False)
-    print(f"Saved pruning vocab q50 to {file_path}. Vocab size: {len(vocab_q50)}")
-
-    vocab_q75 = filtered_df.sort_values(by='weighted_H19', ascending=False).head(int(0.75 * len(filtered_df)))
-    file_path = os.path.join(args.output_dir, "pruning_vocab_q75.csv")
-    vocab_q75['token_id'].to_csv(file_path, index=False)
-    print(f"Saved pruning vocab q75 to {file_path}. Vocab size: {len(vocab_q75)}")
-
-    vocab_q100 = filtered_df = filtered_df
-    file_path = os.path.join(args.output_dir, "pruning_vocab_q100.csv")
-    vocab_q100['token_id'].to_csv(file_path, index=False)
-    print(f"Saved pruning vocab q100 to {file_path}. Vocab size: {len(vocab_q100)}")
-
-    breakpoint()
 
 if __name__ == "__main__":
     main()
